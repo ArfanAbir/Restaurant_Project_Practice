48e5c4b4d67c17e289c602fa481095bcc3fe2909
Arfan Abir <arfanislamabir@gmail.com>
2020-12-19 19:02:04 +0600

    Designing Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Card , CardImg , CardImgOverlay , CardBody , CardTitle } from 'reactstrap';
 
 
 
 
 const MenuItem =props =>{
-    console.log(props);
     return(
         <div>
-            
+           <Card style={{margin:"15px"}}>
+               <CardBody>
+                   <CardImg width="100%" alt ={props.dish.name} src={props.dish.image}
+                    style={{opacity:"0.6"}} />
+                   <CardImgOverlay>
+                       <CardTitle>
+                           {props.dish.name}
+                       </CardTitle>
+                   </CardImgOverlay>
+               </CardBody>
+           </Card>
         </div>
     );
 }
